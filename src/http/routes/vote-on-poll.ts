735629f6f135c01b909fbb5fdcb326a3f046ee6d
adrianovolter <adriano.j.volter@hotmail.com>
2024-02-06 20:41:34 -0300

    função votos

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,22 +1,35 @@
-import { z } from "zod"
-import { prisma } from "../../lib/prisma"
-import { FastifyInstance } from "fastify"
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify";
 
 export async function voteOnPoll(app: FastifyInstance) {
-  app.post('/polls/:pollId/votes', async (request, reply) => {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
     const voteOnPollBody = z.object({
-     pollOptionId: z.string().uuid(),
-    })
+      pollOptionId: z.string().uuid(),
+    });
 
     const voteOnPollParams = z.object({
-        pollId: z.string().uuid(),
-        })
+      pollId: z.string().uuid(),
+    });
 
-    const { pollId } = voteOnPollParams.parse(request.params)
-    const { pollOptionId } = voteOnPollBody.parse(request.body)
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
 
+    let {sessionID} = request.cookies;
+
+    if (!sessionID) {
+      sessionID = randomUUID();
+      reply.setCookie("sessionID", sessionID, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+        signed: true,
+        httpOnly: true,
+      });
+    }
     
-    return reply.status(201).send()
-  })
-
+    return reply.status(201).send({
+        sessionID,
+        });
+  });
 }
