eb126c95d4a9669ca853e22804769b45d7985d06
adrianovolter <adriano.j.volter@hotmail.com>
2024-02-05 23:25:59 -0300

    primas, migrate inicial

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify';
 
 const app = fastify();
 
+app.get('/', async (request, reply) => {
+    return { hello: 'world' };
+});
+
 app.listen({ port: 3333}).then(() => {
     console.log('Server is running on port 3333');
     }
