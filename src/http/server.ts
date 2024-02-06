ea476fd024ef578a8756ba357859cfeb3e43c608
adrianovolter <adriano.j.volter@hotmail.com>
2024-02-06 10:20:42 -0300

    criando as opiçoes da enquete

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,10 @@
 import fastify from 'fastify';
-import { PrismaClient} from '@prisma/client';
-import { z } from 'zod';
+import { createPoll } from './routes/create-poll';
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-
+app.register(createPoll);
+ 
 app.listen({ port: 3333}).then(() => {
     console.log(`Server is running on port http://localhost:3333`);
     }
