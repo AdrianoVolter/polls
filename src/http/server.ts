e7d2eba64d0fe04eb5167fbde05b61f97af935f2
adrianovolter <adriano.j.volter@hotmail.com>
2024-02-06 09:01:39 -0300

    criando modulo rotas

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,24 +7,6 @@ const app = fastify();
 const prisma = new PrismaClient();
 
 
-app.post('/polls', async (request, reply) => {
-
-    const createPollBody = z.object({
-       title: z.string(),
-    });
-    
-    const { title } = createPollBody.parse(request.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        },
-    });
-  
-    return reply.status(201).send({pollId: poll.id});
-}
-);
-
 app.listen({ port: 3333}).then(() => {
     console.log(`Server is running on port http://localhost:3333`);
     }
